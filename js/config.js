dba7c3739f417d5fe65071950806e06f64dd5ebd
hakicoffeelabph <hakicoffeelabph@gmail.com>
2026-08-09 21:09:14 +0400

    Add frontend API configuration

---
+++ js/config.js
@@ -0,0 +1,9 @@
+/**
+ * ==========================================================
+ * HAKI CAFE SYSTEM
+ * FRONTEND CONFIGURATION
+ * ==========================================================
+ */
+
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbzlVNV2cvhCzT_F9fJeKigeOFu3_O47uuXQF7yb5Dk3AndwxbDpwwyu0TCQE0L9QDDk2Q/exec";
